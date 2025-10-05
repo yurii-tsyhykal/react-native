2347b404ac6762536190f451e13dc75cb5b83ea3
Yurii <y.tsyhykal@gmail.com>
2025-10-05 20:59:45 +0200

    started localize login pscreen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -4,10 +4,27 @@ import {ScreenNames} from '../constants/screenNames';
 import {RootStackNavigation} from './types';
 import LoggedInStack from './LoggedInStack';
 import LoggedOutStack from './LoggedOutStack';
+import {useEffect, useState} from 'react';
+import {getAuth, onAuthStateChanged} from '@react-native-firebase/auth';
+import {ActivityIndicator, View} from 'react-native';
 
 const Stack = createNativeStackNavigator<RootStackNavigation>();
 
 export default function RootNavigation() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), user => {
+      setIsAuthenticated(!!user);
+    });
+    return subscriber;
+  }, []);
+  if (isAuthenticated === null) {
+    return (
+      <View>
+        <ActivityIndicator size={'large'} color="#7A71BA" />
+      </View>
+    );
+  }
   return (
     <NavigationContainer
       theme={{
@@ -18,18 +35,18 @@ export default function RootNavigation() {
         },
       }}
     >
-      <Stack.Navigator
-        initialRouteName={ScreenNames.LOGGED_IN_STACK}
-        screenOptions={{headerShown: false}}
-      >
-        <Stack.Screen
-          name={ScreenNames.LOGGED_IN_STACK}
-          component={LoggedInStack}
-        />
-        <Stack.Screen
-          name={ScreenNames.LOGGED_OUT_STACK}
-          component={LoggedOutStack}
-        />
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        {isAuthenticated ? (
+          <Stack.Screen
+            name={ScreenNames.LOGGED_IN_STACK}
+            component={LoggedInStack}
+          />
+        ) : (
+          <Stack.Screen
+            name={ScreenNames.LOGGED_OUT_STACK}
+            component={LoggedOutStack}
+          />
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screen/Auth/components/AuthHeader/index.tsx
+++ src/screen/Auth/components/AuthHeader/index.tsx
@@ -5,6 +5,7 @@ import {useNavigation} from '@react-navigation/native';
 import {ScreenNames} from '../../../../constants/screenNames';
 import {RootStackNavigation} from '../../../../navigation/types';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {useTranslation} from 'react-i18next';
 
 export interface IAuthHeader {
   activeTab: 'login' | 'Registration';
@@ -13,30 +14,35 @@ export interface IAuthHeader {
 export default function AuthHeader({activeTab}: IAuthHeader) {
   const navigate = useNavigation<StackNavigationProp<RootStackNavigation>>();
 
-  const navigateToLogin = () => navigate.navigate(ScreenNames.LOGIN_PAGE);
+  const {t} = useTranslation();
+  const navigateToLogin = () =>
+    navigate.navigate('LOGGED_OUT_STACK', {
+      screen: ScreenNames.LOGIN_PAGE,
+    });
   const navigateToRegistration = () =>
-    navigate.navigate(ScreenNames.Registration_PAGE);
+    navigate.navigate('LOGGED_OUT_STACK', {
+      screen: ScreenNames.REGISTRATION_PAGE,
+    });
 
   return (
     <>
       <View style={styles.titleContainer}>
-        <Text style={styles.title}>Раді тебе вітати!</Text>
-        <Text style={styles.welcomeText}>
-          Koжен пухнастик заслуговує на дбайливих господарів.{'\n'}
-          <Text>Ми допоможемо тобі знайти друга.</Text>
-        </Text>
+        <Text style={styles.title}>{t('auth.authHeader.title')}</Text>
+        <Text style={styles.welcomeText}>{t('auth.authHeader.subtitle')}</Text>
       </View>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           onPress={navigateToLogin}
-          style={activeTab === 'login' ? styles.activeTab : styles.disabledTab}>
+          style={activeTab === 'login' ? styles.activeTab : styles.disabledTab}
+        >
           <Text style={styles.authText}>Вхід</Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={navigateToRegistration}
           style={
             activeTab === 'Registration' ? styles.activeTab : styles.disabledTab
-          }>
+          }
+        >
           <Text style={styles.authText}>Реєстрація</Text>
         </TouchableOpacity>
       </View>
